26ffbf7e928754385565be1e630bd8408c7ad192
Bill Abel <billabel@me.com>
2018-09-11 09:52:41 -0500

    Initial commit with pseudo code

---
+++ fibonacci.rb
@@ -0,0 +1,10 @@
+# find the nth value in the fibonacci squence
+#
+# get the user input for n, the nth value to find
+#
+# if n = 0, output 0
+# if n = 1, output 1
+#
+# if n > 2, output the sum of the previous two values
+#
+# if n < 0, output the sum of the previous two values
