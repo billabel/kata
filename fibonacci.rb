2fbd62998e9c6d733c1c1ae1d50458818be2ee95
Bill Abel <billabel@me.com>
2018-09-11 10:44:42 -0500

    Linear approach
    - switched to linear approach
    - can calculate up to 1,000,000 in 30 seconds
    - can calculate negative odd numbers, but not negative even numbers:x

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,20 +4,35 @@
 require 'Benchmark'
 time = Benchmark.realtime do
 
-# store the calculated fibonacci numbers
-@cache = {}
-@cache[0] = 0
-@cache[1] = 1
-
 # get the user input for n, the nth value to find
 n = gets.to_i
 
 def fib(n)
 # if n = 0, output 0
+  if n == 0
+    return 0
 # if n = 1, output 1
 # if n > 2, output the sum of the previous two values
+  elsif n > 0
+    a = 0
+    b = 1
+    n.times do
+      c = a
+      a = b
+      b = c + b
+    end
+    return a
 # if n < 0, output the sum of the previous two values
-  @cache[n] ||= fib(n-1) + fib(n-2)
+  elsif n < 0
+    a = 0
+    b = 1
+    n.abs.times do
+      c = a
+      a = b
+      b = c + b
+    end
+    return (a * (-1)**n)
+  end
 end
 
 puts fib(n).to_s
